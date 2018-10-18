8ba001feb0860405c38a321cc54bfe9c44e8144c
Elnura <elnuraarslan@gmail.com>
2018-10-18 16:40:29 +0300

    Update cam.py

--- object_detection_app/cam.py
+++ object_detection_app/cam.py
@@ -1,9 +1,20 @@
-import urllib3
-import cv2
-import numpy as np
-from io import StringIO
-from PIL import Image
-import io
+
+try:
+  import logging
+  import traceback
+
+  logging.basicConfig(filename='Human_Detection_Logger.txt', level=logging.DEBUG,
+                      format="%(asctime)s:%(levelname)s:%(message)s")
+  import urllib3
+  import cv2
+  import numpy as np
+  from io import StringIO
+  from PIL import Image
+  import io
+except Exception as e:
+  #print("type error: " + str(e))
+  #print(traceback.format_exc())
+  logging.debug("message: {}".format(traceback.format_exc()))
 
 class ipCamera:
     def __init__(self, url, user=None, password=None):
@@ -13,7 +24,8 @@ class ipCamera:
             headers = urllib3.util.make_headers(basic_auth='{}:{}'.format(user, password))
             http.headers = headers
             self.http = http
-        except:
+        except Exception as e:
+            logging.debug("message: {}".format(traceback.format_exc()))
             print('Error occurred:') # log error message with time occured
 
     def get_frame(self):
@@ -22,7 +34,8 @@ class ipCamera:
             resized_image = Image.open(io.BytesIO(r.data))
             frame = np.array(resized_image)
             return frame
-        except:
+        except Exception as e:
+           logging.debug("message: {}".format(traceback.format_exc()))
            return None # change it to an empty object of image
 
 #url = 'http://172.21.2.254/fastjpeg?stamp=0.383161508780318.jpg'
